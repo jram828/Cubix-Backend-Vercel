e5be0924f654bb9f4d6e46807ff7f67154ff6edf
jram828 <128335805+jram828@users.noreply.github.com>
2024-08-07 22:01:17 -0500

    fix auth handler

--- Server/src/routes/userRoutes.js
+++ Server/src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { registerUserHandler } from "../handlers/userHandler.js";
-import { authHandler } from "../handlers/authhandler.js";
+import { authHandler } from "../handlers/authHandler.js";
 import recoveryPasswordHandler from "../handlers/postPasswordRecoveryHandler.js";
 import {
   findOneUser,
